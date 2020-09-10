fa3a524915b0c14f6d5378832e60eeba59a41d96
Tai Masuda <68070864+tmasuda14@users.noreply.github.com>
2020-09-09 17:19:55 -0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((noteInfo) => {
+        return (
+          <Note
+            key={noteInfo.key}
+            title={noteInfo.title}
+            content={noteInfo.content}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
